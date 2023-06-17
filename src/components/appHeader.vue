fe2f739f7cd7c95ad6a0d051fd2b186fd097d8d1
LouisAssaneFaye <louis.faye.18@outlook.it>
2023-06-17 18:55:59 +0200

    header terminato

--- src/components/appHeader.vue
+++ src/components/appHeader.vue
@@ -2,15 +2,33 @@
     <header>
         
         <div class="header-up">
+            <img src="../assets/img/dc-logo.png" alt="logo">
+            <ul>
+                <li>CHARACTERS</li>
+                <li>COMICS</li>
+                <li>MOVIES</li>
+                <li>TV</li>
+                <li>GAMES</li>
+                <li>COLLECTIBLES</li>
+                <li>VIDEOS</li>
+                <li>FANS</li>
+                <li>NEWS</li>
+                <li>SHOP</li>
+            </ul>
 
         </div>
         
         <div class="header-down">
+            <span>
+                -- Content goes here --
+            </span>
 
         </div>
         
     </header>
 </template>
+
+
 <script>
 export default {
     name:'appHeader',
@@ -21,12 +39,47 @@ export default {
     },
     methods:{
 
-    }
-    
+    } 
 }
 </script>
+
+
 <style lang="scss">
 @use '../styles/partials/variables' as *;
 @use '../styles/partials/mixins' as *;
+
+header{
+    div.header-up{
+        height: 70px;
+        @include flexOne();
+        img{
+            height: 50px;
+            width: 50px;
+        }
+        
+        ul>li{
+            font-size: 0.6rem;
+            list-style-type: none;
+            margin-left: 15px;
+            
+        }
+
+        ul{
+            display: flex;
+        }
+    }
+
+    div.header-down{
+            height: 70px;
+            background-color: $blackColor;
+            padding-top: 1.35rem;
+            padding-left: 14rem;
+            span {
+                display: inline-block;
+                color: $whiteColor;
+                font-size: 1.6rem;
+            }
+        }
+}
     
 </style>
