9511cfb4dc1a85bc8b00a0115b70859ed59e0a7c
LouisAssaneFaye <louis.faye.18@outlook.it>
2023-06-18 18:59:38 +0200

    accoppiamento dei vari file

--- src/components/singleFilm.vue
+++ src/components/singleFilm.vue
@@ -1,7 +1,15 @@
 <template lang="">
-    <div>
-        
-    </div>
+    <div class="product">
+                        <div class="img">
+                            <img src="https://www.panini.it/media/catalog/product/cache/a5b5dd3adfe0d321084804c738f29601/M/1/M1BLLA015ISBN_0.jpg" alt="cover">
+                            
+                        </div>
+                        <div class="description">
+                            <span>
+                                AMERICAN VAMPIRE 1976
+                            </span>
+                        </div>
+                    </div>
 </template>
 <script>
 export default {
